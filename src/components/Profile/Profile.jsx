f40667b70bedba9d565bc01b4826e919bdb44496
Andrew <deleniv08@gmail.com>
2022-10-16 18:28:09 +0200

    update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import PropTypes, { string } from "prop-types";
+import PropTypes from "prop-types";
 import css from './Profile.module.css'
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
